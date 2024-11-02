63fc68764fdd7b797b8403582c4a2bc6df28f7d8
Sayali Suresh Gajare <Sayali.Gajare11@gmail.com>
2024-11-02 18:06:51 +0530

    added the modified Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args){
         int a = 5;
         int b = 5;
-        System.out.println("The sum of a and b "+ (a+b));
+        System.out.println((a+b));
     }
 }
